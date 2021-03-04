fc45f53a75a6a29d8b66b3e0def88e779e636e31
Zahra Bodaghi-Ginns <zbodaghi@CougarNet.UH.EDU>
2021-03-03 18:17:27 -0600

    Upload the classes

---
+++ Assignment2/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Client
+{
+	public Client()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+		//Validation Function()  
+		//get and set for functions
+		//members
+		//
+	}
+}
